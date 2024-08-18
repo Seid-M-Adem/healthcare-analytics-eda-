efa2870ac7db998c4a35ca89474b837545a8ae24
Seid Adem <50076112+Seid-M-Adem@users.noreply.github.com>
2024-08-18 21:35:18 +0200

    Update data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,1 +1,15 @@
+import pandas as pd
+
+def load_data(filepath):
+    """Load data from a CSV file."""
+    return pd.read_csv(filepath)
+
+def clean_data(df):
+    """Clean the data by handling missing values."""
+    df.fillna(df.mean(), inplace=True)
+    return df
+
+def save_data(df, filepath):
+    """Save cleaned data to a CSV file."""
+    df.to_csv(filepath, index=False)
 
