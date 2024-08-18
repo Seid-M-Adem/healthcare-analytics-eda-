e8791bdd5abfeae70394d2eb3c3b77d5ca0c10fb
Seid Adem <50076112+Seid-M-Adem@users.noreply.github.com>
2024-08-18 21:37:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='healthcare-analytics-eda',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'jupyter',
+    ],
+    description='Exploratory Data Analysis of Healthcare Data',
+    author='Seid M Adem',
+    author_email='seid.adem@gmx.ch',
+)
 
