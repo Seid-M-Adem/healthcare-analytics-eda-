1eb123b09c7f32c406e519283a429ac2bd474749
Seid Adem <50076112+Seid-M-Adem@users.noreply.github.com>
2024-08-18 23:19:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,27 @@ from setuptools import setup, find_packages
 setup(
     name='healthcare-analytics-eda',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(),  # Automatically finds all packages and sub-packages
     install_requires=[
-        'pandas',
-        'numpy',
-        'matplotlib',
-        'seaborn',
-        'scikit-learn',
+        'pandas',      # Data manipulation library
+        'matplotlib',  # Plotting library
+        'seaborn',     # Statistical data visualization library
+        'jupyter',     # Jupyter notebook support
     ],
-    entry_points={
-        'console_scripts': [
-            'data_processing=src.data_processing:main',
-            'visualization=src.visualization:main',
-        ],
-    },
+    description='Exploratory Data Analysis of Healthcare Data',
+    author='Seid-M-Adem',                    # Replace with your name
+    author_email='seid.adem@gmx.ch',  # Replace with your email address
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    python_requires='>=3.6',  # Specify the minimum Python version required
 )
